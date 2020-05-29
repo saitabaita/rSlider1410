c25f7d02d42d3697ccdc62ca4fe37d46cdc96c2b
saitabaita <45140900+saitabaita@users.noreply.github.com>
2020-05-29 14:44:08 +0700

    Изменил способ подключения слайдера - через глобальную функцию jQuery

--- index.ts
+++ index.ts
@@ -7,26 +7,27 @@ import { rsModel } from './src/libs/rs-model';
 import { rsView } from './src/libs/rs-view';
 import { rsController } from './src/libs/rs-controller';
 
-let pluginId: number = 0;
-
-function rSlider1410(wh:string, min: number=0, max: number=1000, step: number=10, val: number=200, isVertical: boolean, isTwo: boolean){
-    let element: JQuery<HTMLElement> = $("<input type='range' max='"+max+"' step='"+step+"' value='"+val+"' min='"+min+"'>").appendTo($(".mySliders"));
-    let model: rsModel = new rsModel(element, isTwo);
-    let view: rsView;
-    if(isVertical===true){
-        view = new rsView(isTwo, isVertical, pluginId++, element, $('<div style="height:'+wh+';width:20px;margin:30px"/>').appendTo($(".mySliders")));
-    }else{
-        view = new rsView(isTwo, isVertical, pluginId++, element, $('<div style="padding:20px;max-width:'+wh+';margin-bottom:30px"/>').appendTo($(".mySliders")));
+$.fn.extend({
+    addSlider1410: function (wh:string, min: number=0, max: number=1000, step: number=10, val: number=200, isVertical: boolean, isTwo: boolean) {
+        return this.each(function () {
+            const $this = $(this);
+            let element: JQuery<HTMLElement> = $("<input type='range' max='"+max+"' step='"+step+"' value='"+val+"' min='"+min+"'>").appendTo($this); 
+            let model: rsModel = new rsModel(element, isTwo);
+            let view: rsView;
+            if(isVertical===true){
+                view = new rsView(isTwo, isVertical, pluginId++, element, $('<div style="height:'+wh+';width:20px;margin:30px"/>').appendTo($this));
+            }else{
+                view = new rsView(isTwo, isVertical, pluginId++, element, $('<div style="padding:20px;max-width:'+wh+';margin-bottom:30px"/>').appendTo($this));
+            }
+            let controller: rsController = new rsController(model, view);
+        });
     }
-    let controller: rsController = new rsController(model, view);
-}
-
-$(document).ready(function(){
-    rSlider1410('300px', 5, 400, 5, 100, true, false);
-    rSlider1410('800px', 10, 1000, 10, 300, false, true);
-    rSlider1410('800px', 0, 1000, 1, 300, false, false);
-    //rSlider1410('600px', 0, 1000, 10, 300, false);
-
 });
+let pluginId: number = 0; // номер слайдера
+
+const jQuerySlider = $(".mySliders");
+jQuerySlider.addSlider1410('300px', 5, 400, 5, 100, true, false);
+jQuerySlider.addSlider1410('800px', 10, 1000, 10, 300, false, true);
+jQuerySlider.addSlider1410('800px', 0, 1000, 1, 300, false, false);
 
 
---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+declare function addSlider1410(wh:string, min: number, max: number, step: number, val: number, isVertical: boolean, isTwo: boolean): JQuery<HTMLElement>;
+interface JQuery {
+    addSlider1410(wh:string, min: number, max: number, step: number, val: number, isVertical: boolean, isTwo: boolean): JQuery;
+}
